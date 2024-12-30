434c693ffe5fffe7f7cd0191e58f9edbbcaca89a
Anish Baniya <anishbaniya07@gmail.com>
2024-12-30 21:14:34 +0545

    check

--- Dart/fibonacci.dart
+++ Dart/fibonacci.dart
@@ -1,14 +1,14 @@
-void main(){
-  int a=1;
-  int b=2;
-  int total=0;
-  while(b<35){
-    if(b % 2==0){
-      total=total+b;
-      }
-      int next=a+b;
-      a=b;
-      b=next;
+void main() {
+  int a = 1;
+  int b = 2;
+  int total = 0;
+  while (b < 35) {
+    if (b % 2 == 0) {
+      total = total + b;
+    }
+    int next = a + b;
+    a = b;
+    b = next;
   }
-    print(total);
+  print(total);
 }
