7a8c54308771bd921ff42f2a1ebd2d7c01569e33
Anish Baniya <anishbaniya07@gmail.com>
2025-04-21 11:47:42 +0545

    learn: package cache netwrok

--- Flutter/builder_proj/lib/pages/grid_page.dart
+++ Flutter/builder_proj/lib/pages/grid_page.dart
@@ -1,14 +1,25 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-class GridPage extends StatelessWidget {
+class GridPage extends StatefulWidget {
   const GridPage({super.key});
+
+  @override
+  State<GridPage> createState() => _GridPageState();
+}
+
+class _GridPageState extends State<GridPage> {
+  final List<String> imageUrls = List.generate(
+    10,
+    (index) => 'https://picsum.photos/id/${index + 10}/200/200',
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('GridView Demo'))),
       body: GridView.builder(
-        itemCount: 10,
+        itemCount: imageUrls.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2, //items per row
           mainAxisSpacing: 10, //vertical space
@@ -18,7 +29,14 @@ class GridPage extends StatelessWidget {
           return Container(
             color: Colors.amber,
             child: Center(
-              child: Text('Item $index', style: TextStyle(fontSize: 18)),
+              child: CachedNetworkImage(
+                imageUrl: imageUrls[index],
+                placeholder:
+                    (context, url) =>
+                        Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                fit: BoxFit.cover,
+              ),
             ),
           );
         },
